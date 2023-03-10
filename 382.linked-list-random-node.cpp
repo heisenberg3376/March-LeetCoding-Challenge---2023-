fd27d83a6c08ce613eccf75810d09766c25a4ce3
heisenberg3376 <kattaphanendrasairam@gmail.com>
2023-03-10 22:25:50 +0530

    cplusplus

---
+++ 382.linked-list-random-node.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=382 lang=cpp
+ *
+ * [382] Linked List Random Node
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>v;
+    Solution(ListNode* head) {
+        ListNode *temp = head;
+        while(temp){
+            v.push_back(temp->val);
+            temp = temp->next;
+        }
+    }
+    
+    int getRandom() {
+        int a = rand()%(v.size());
+        return v[a];
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
+// @lc code=end
+
